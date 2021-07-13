767b54159c7ff8c4415159192a60d90f91db19dd
wlrhkd49 <wlrhkd49@naver.com>
2021-07-13 17:58:14 +0900

    부품찾기 계수정렬 활용

---
+++ Binary Search/부품찾기_계수정렬.py
@@ -0,0 +1,19 @@
+# 가게의 부품 개수 입력
+n = int(input())
+array = [0] * 1000000
+
+# 가게에 있는 전체 부품 번호를 입력받아서 기록
+for i in input().split():
+    array[int(i)] = 1
+
+# 손님이 확인 요청한 부품 개수을 입력
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
+x = list(map(int,input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    if array[i] == 1:
+        print('yes', end = ' ')
+    else:
+        print('no', end = ' ')
