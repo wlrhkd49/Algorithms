540d57a5be85c6351e95bbc31eb35d6cc932ed16
wlrhkd49 <wlrhkd49@naver.com>
2021-07-21 14:13:01 +0900

    개선된 다익스트라 구현 수정

--- Shortest Path/숨바꼭질.py
+++ Shortest Path/숨바꼭질.py
@@ -30,7 +30,7 @@ def dijkstra(start):
         for i in graph[now]:
             cost = dist + i[1]
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if distance[i[0]] > cost:
+            if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 
