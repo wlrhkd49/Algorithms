12a6e2a33f03c79dce7e03063ae58406fed86e71
wlrhkd49 <wlrhkd49@naver.com>
2021-08-27 15:13:15 +0900

    외벽점검

---
+++ Implementation/Q14.py
@@ -0,0 +1,28 @@
+from itertools import permutations
+
+def solution(n, weak, dist):
+    # 길이를 2배로 늘려서 '원형'을 일자 형태로 변환
+    length = len(weak)
+    for i in range(length):
+        weak.append(weak[i]+n)
+    answer = len(dist) + 1 # 투입할 친구 수의 최솟값을 위해 설정
+    
+    # 0 ~ length-1까지 위치를 시작점으로 설정
+    for start in range(length):
+        # 친구를 나열하는 모든 경우의 수를 각각에 대해 확인
+        for friends in list(permutations(dist, len(dist))):
+            count = 1 # 투입할 친구 수
+            # 해당 친구가 점검할 수 있는 마지막 위치
+            position = weak[start] + friends[count - 1]
+            # 시작점부터 모든 취약 지점을 확인
+            for index in range(start, start+length):
+                # 점검할 수 있는 위치를 벗어나는 경우
+                if position < weak[index]:
+                    count += 1 # 새로운 친구 투입
+                    if count > len(dist): # 더 이상 투입이 불가능하면 종료
+                        break
+                    position = weak[index] + friends[count-1]
+            answer = min(answer, count) # 최솟값 계산
+    if answer > len(dist):
+        return -1
+    return answer
