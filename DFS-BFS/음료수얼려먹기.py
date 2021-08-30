0f47949de03eed7d16927ba2cbf8d1528dae2996
wlrhkd49 <wlrhkd49@naver.com>
2021-08-30 10:47:05 +0900

    DFS 복습

--- DFS-BFS/음료수얼려먹기.py
+++ DFS-BFS/음료수얼려먹기.py
@@ -1,30 +1,35 @@
 n, m = map(int, input().split())
+
+# 2차원 리스트 맵 정보 입력
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs(x, y):
-    if x<=-1 or x>=n or y<=-1 or  y>=m:
+def dfs(x,y):
+
+    # 주어진 범위를 벗어나면 종료
+    if x<=-1 or y<=-1 or x>=n or y>=m:
         return False
-    
-    # 아직 노드 방문 안했다면
-    if graph[x][y]==0:
-        # 방문 처리
-        graph[x][y]=1
-        # 상, 하, 좌, 우 위치도 모두 재귀적 호출
-        dfs(x-1,y)
-        dfs(x+1,y)
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문처리
+        graph[x][y] = 1
+        # 상하좌우도 재귀적으로 호출
+        # 연결된 모든 지점 방문
         dfs(x,y-1)
         dfs(x,y+1)
+        dfs(x-1,y)
+        dfs(x+1,y)
         return True
     return False
 
-# 모든 노드(위치)에 대하여 음료수 채우기
 result = 0
+
 for i in range(n):
     for j in range(m):
+        # graph[i][j] == 0 이라면 방문 시작
         if dfs(i,j) == True:
             result+=1
 
-print(result) # 정답 출력
+print(result)
