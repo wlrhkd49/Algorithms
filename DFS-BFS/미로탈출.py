cfd7965eb966af0e934e7977c860105aac6eaf41
wlrhkd49 <wlrhkd49@naver.com>
2021-08-30 10:58:41 +0900

    BFS 복습

--- DFS-BFS/미로탈출.py
+++ DFS-BFS/미로탈출.py
@@ -10,39 +10,35 @@ for i in range(n):
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-# bfs 함수 정의
-def bfs(x,y):
-    # 큐 구현을 위해 dequeu 라이브러리 사용
+# BFS 구현
+def bfs(x, y):
+    # 큐 구현을 위해 deque 사용
     queue = deque()
     queue.append((x,y))
 
-    #큐 빌 때까지 반복
+    # 큐가 빌 때까지 반복
     while queue:
-        x,y = queue.popleft()
-        # 현재 위치에서 네 방향으로의 위치 확인
+        x, y = queue.popleft()
+        # 현재 위치에서 4방향 위치 확인
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            
-            # 미로 공간을 벗어난 경우 무시
-            if nx<0 or nx>=n or ny<0 or ny>=m:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 그래프 범위 벗어나는 경우 무시
+            if nx<0 or ny<0 or nx>=n or ny>=m:
                 continue
 
-            # 괴물 만난 경우 무시
+            # 벽인 경우 무시
             if graph[nx][ny] == 0:
                 continue
-            
-            # 해당 노드 처음 방문한 경우 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y]+1
-                queue.append((nx, ny))
 
-    # 가장 오른쪽 하단 최단 거리 반환
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    
+    # 그래프의 우측하단 값 리턴
     return graph[n-1][m-1]
 
 print(bfs(0,0))
-
-
-
-
 
