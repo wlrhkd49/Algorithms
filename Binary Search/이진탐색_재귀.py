cd24c090af1b5d8e12f8ae7b3cb949d0d52e849d
wlrhkd49 <wlrhkd49@naver.com>
2021-09-04 18:18:41 +0900

    이진탐색 재귀 복습

--- Binary Search/이진탐색_재귀.py
+++ Binary Search/이진탐색_재귀.py
@@ -1,25 +1,25 @@
+# 이진 탐색 재귀
 def binary_search(array, target, start, end):
     if start > end:
-        return None
-    mid = (start + end)//2
+        None
+    mid = (start+end)//2
     # 찾은 경우 중간점 인덱스 반환
-    if target == array[mid]:
+    if array[mid] == target:
         return mid
-    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
-    elif target < array[mid]:
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확ㅇ니
+    elif array[mid] > target:
         return binary_search(array, target, start, mid - 1)
-    # 중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
     else:
         return binary_search(array, target, mid + 1, end)
 
-# n(원소의 개수)와 target 입력 받기
+# 원소의개수와 타겟을 입력받기
 n, target = list(map(int, input().split()))
-# 전체 원소 입력 받기
+# 전체 원소 입력
 array = list(map(int, input().split()))
 
 # 이진 탐색 수행 결과 출력
 result = binary_search(array, target, 0, n-1)
 if result == None:
-    print("원소가 존재하지 않습니다.")
+    print("원소 존재 x")
 else:
-    print(result + 1)
+    print(result+1)
