cf3eecd286701e7c086952e07878af4cae03c6d4
wlrhkd49 <wlrhkd49@naver.com>
2021-09-04 18:18:49 +0900

    이진탐색 반복 복습

--- Binary Search/이진탐색_반복.py
+++ Binary Search/이진탐색_반복.py
@@ -1,15 +1,15 @@
 def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
+    while start<=end:
+        mid = (start+end)//2
         # 찾은 경우 중간점 인덱스 반환
         if array[mid] == target:
             return mid
-        # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
         elif array[mid] > target:
             end = mid - 1
-        # 중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
         else:
             start = mid + 1
+    return None
+    
 
 # n(원소의 개수)와 target 입력 받기
 n, target = list(map(int, input().split()))
