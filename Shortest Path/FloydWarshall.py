fb3c11b29e38dafbcd7dfd37250037581fcce49c
wlrhkd49 <wlrhkd49@naver.com>
2021-09-15 17:59:10 +0900

    플로이드 워셜 알고리즘 복습

--- Shortest Path/FloydWarshall.py
+++ Shortest Path/FloydWarshall.py
@@ -4,22 +4,22 @@ INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 n = int(input())
 m = int(input())
 
-# 2차원 리스트(그래프 표현)을 만들고, 모든 값을 무한으로 초기화
+# 2차원 리스트를 만들고, 모든 값을 무한으로 초기화
 graph = [[INF] * (n+1) for _ in range(n+1)]
 
-# 자기 자신에게서 자기 자신으로 가는 비용은 0으로 초기화
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
 for a in range(1, n+1):
     for b in range(1, n+1):
-        if a == b:
+        if a==b:
             graph[a][b] = 0
 
 # 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
 for _ in range(m):
     # A에서 B로 가는 비용은 C라고 설정
-    a, b, c = map(int, input().split())
+    a, b, c = map(int , input().split())
     graph[a][b] = c
 
-# 점화식에 따라 플로이드 워셜 알고리즘 수행
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
 for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
@@ -28,10 +28,10 @@ for k in range(1, n+1):
 # 수행된 결과를 출력
 for a in range(1, n+1):
     for b in range(1, n+1):
-        # 도달할 수 없는 경우, 무한이라고 출력
+        # 도달할 수 없는 경우, 무한으로 출력
         if graph[a][b] == INF:
-            print("INFINITY", end = ' ' )
-        # 도달할 수 있는 경우 거리를 출력
+            print("INFINITY", end=' ')
+
         else:
             print(graph[a][b], end=' ')
     print()
