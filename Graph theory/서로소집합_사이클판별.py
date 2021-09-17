7bf1453af2f6c360f8deb9617ad51ce61432f29d
wlrhkd49 <wlrhkd49@naver.com>
2021-09-17 15:49:32 +0900

    서로소 집합 알고리즘 복습

--- Graph theory/서로소집합_사이클판별.py
+++ Graph theory/서로소집합_사이클판별.py
@@ -1,13 +1,10 @@
 # 간선에 방향성이 없는 무향 그래프에서 사용
 
-# 특정 원소가 속한 집합을 찾기
 def find_parent(parent, x):
-    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-# 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
@@ -16,11 +13,10 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-# 노드의 개수와 간선의 개수 입력받기
+# 노드의 개수와 간선의 개수 입력
 v, e = map(int, input().split())
-parent = [0] * (v+1) # 부모 테이블 초기화
+parent = [0] * (v+1)
 
-# 부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(1, v+1):
     parent[i] = i
 
@@ -28,7 +24,6 @@ cycle = False # 사이클 발생 여부
 
 for i in range(e):
     a, b = map(int, input().split())
-    # 사이클이 발생한 경우 종료
     if find_parent(parent, a) == find_parent(parent, b):
         cycle = True
         break
@@ -36,6 +31,6 @@ for i in range(e):
         union_parent(parent, a, b)
 
 if cycle:
-    print("사이클 발생 o")
+    print('사이클 발생')
 else:
-    print("사이클 발생 x")
+    print('사이클 발생 x')
