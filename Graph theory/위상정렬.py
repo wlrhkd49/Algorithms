cd60e9b7eec920deeabb1da0f9480badb36f7f71
wlrhkd49 <wlrhkd49@naver.com>
2021-09-17 16:29:25 +0900

    위상 정렬 복습

--- Graph theory/위상정렬.py
+++ Graph theory/위상정렬.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 # 노드의 개수와 간선의 개수를 입력
 v, e = map(int, input().split())
 # 모든 노드에 대한 진입차수는 0으로 초기화
@@ -9,45 +10,31 @@ graph = [[] for i in range(v+1)]
 # 방향 그래프의 모든 간선 정보를 입력
 for _ in range(e):
     a, b = map(int, input().split())
-    graph[a].append(b) # 정점 A에서 B로 이동 가능
-    # 진입차수를 1 증가
+    graph[a].append(b)
+    # 진입차수를 1증가
     indegree[b] += 1
 
 # 위상 정렬 함수
 def topology_sort():
-    result = [] # 알고리즘 수행 결과를 담을 리스트
-    q = deque() # 큐 기능을 위한 deque 라이브러리 사용
+    result = []
+    q = deque()
 
-    # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
     for i in range(1, v+1):
         if indegree[i] == 0:
             q.append(i)
 
-    # 큐가 빌 때까지 반복
     while q:
-        # 큐에서 원소 꺼내기
         now = q.popleft()
         result.append(now)
-        # 해당 원소와 연결된 노드들의 진입차수에서 1 배기
+        # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
         for i in graph[now]:
             indegree[i] -= 1
-            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            # 새롭게 진입차수가 0이되는 노드를 큐에 삽입
             if indegree[i] == 0:
                 q.append(i)
 
-    # 위상 정렬을 수행한 결과 출력
+        # 위상 정렬을 수행한 결과 ㅜㄹ력
     for i in result:
-        print(i, end = ' ')
-
+        print(i, end=' ')
+    
 topology_sort()
-
-# 입력 예시
-# 7 8
-# 1 2 
-# 1 5
-# 2 3
-# 2 6
-# 3 4
-# 4 7
-# 5 6
-# 6 4
