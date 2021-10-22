8f2b42028536a6ff1cd969532b8659dd8d965523
wlrhkd49 <wlrhkd49@naver.com>
2021-10-22 11:59:33 +0900

    골든바흐의 추측

---
+++ baekjoon/수학/6588.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+# 에라토스테네스의 체
+# 전체 수 만큼 True 리스트 생성
+# 2부터 1씩 더해주면서 그 배수에 해당하는 값들을 False로 바꾼다.
+array = [True for i in range(1000001)]
+
+for i in range(2, 1001):
+    if array[i]:
+        for k in range(i + i, 1000001, i):
+            array[k] = False
+
+while True:
+    n = int(input())
+    if n == 0: break
+    
+    for i in range(3, len(array)):
+        if array[i] and array[n-i]:
+            print(n, "=", i, "+", n-i)
+            break
