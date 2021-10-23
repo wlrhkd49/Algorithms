0925fbcb3c49a8bfb5b9b7b7ae1b56d72ad20235
wlrhkd49 <wlrhkd49@naver.com>
2021-10-23 14:44:26 +0900

    최대공약수와 최소공배수

---
+++ baekjoon/수학/2609.py
@@ -0,0 +1,13 @@
+# 유클리드 호제법
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return (a * b) // gcd(a,b)
+
+a, b = map(int, input().split())
+
+print(gcd(a,b))
+print(lcm(a,b))
