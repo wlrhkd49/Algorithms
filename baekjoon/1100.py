6a51b076d3d3c1892059043aaf2792d086272610
wlrhkd49 <wlrhkd49@naver.com>
2021-11-05 14:40:45 +0900

    하얀 칸

---
+++ baekjoon/1100.py
@@ -0,0 +1,11 @@
+data = [0 for _ in range(8)]
+for i in range(8):
+    data[i] = list(input())
+
+count = 0
+for i in range(8):
+    for j in range(8):
+        if (i+j)%2 == 0:
+            if data[i][j] == 'F':
+                count+=1
+print(count)
