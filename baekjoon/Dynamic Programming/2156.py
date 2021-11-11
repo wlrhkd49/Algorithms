b383e04d14900a714d2ad3f60f46a1cc634b8eca
wlrhkd49 <wlrhkd49@naver.com>
2021-11-11 15:42:29 +0900

    포도주 시식

--- baekjoon/Dynamic Programming/2156.py
+++ baekjoon/Dynamic Programming/2156.py
@@ -1,8 +1,9 @@
-data = []
+data = [0]*10000
 n = int(input())
 for i in range(n):
-    data.append(int(input()))
-dp = [0] * n
+    data[i] = (int(input()))
+
+dp = [0] * 10000
 dp[0] = data[0]
 dp[1] = data[0]+data[1]
 dp[2] = max(dp[1], data[1]+data[2], data[2]+data[0])
@@ -12,4 +13,4 @@ dp[2] = max(dp[1], data[1]+data[2], data[2]+data[0])
 # 전 포도주 안마시고 현재 위치 포도주 마시기
 for i in range(3, n):
     dp[i] = max(dp[i-1], dp[i-3]+data[i-1]+data[i], dp[i-2]+data[i])
-print(dp[n-1])
+print(max(dp))
