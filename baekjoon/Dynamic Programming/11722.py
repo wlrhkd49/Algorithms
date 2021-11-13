1e7046228324e22d09fe49c4d69a478220c0e561
wlrhkd49 <wlrhkd49@naver.com>
2021-11-13 18:01:29 +0900

    가장 긴 감소하는 부분 수열

--- baekjoon/Dynamic Programming/11722.py
+++ baekjoon/Dynamic Programming/11722.py
@@ -1,14 +1,3 @@
-# n = int(input())
-# data = list(map(int, input().split()))
-# dp = [1] * (n+1)
-
-# for i in range(len(data)):
-#     for j in range(i):
-#         if data[j] < data[i]:
-#             dp[i] = max(dp[i], dp[j]+1)
-
-# print(max(dp))
-
 n = int(input())
 
 data = list(map(int, input().split()))
