bc6739d6afb99da0735f4c9dc8bcbb3fa9476847
wlrhkd49 <wlrhkd49@naver.com>
2021-11-19 13:36:20 +0900

    올바른 괄호

---
+++ programmers_level1/올바른괄호.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = True
+    cnt = 0
+    for i in s:
+        if i =='(':
+            cnt+=1
+        else:
+            cnt-=1
+            if cnt < 0:
+                answer = False
+    if cnt != 0:
+        answer = False
+    return answer
