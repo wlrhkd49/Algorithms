bccc6016b3de061c111f98c57956defcc172287b
wlrhkd49 <wlrhkd49@naver.com>
2021-12-08 18:53:36 +0900

    문자열 내의 P와 Y의 개수

---
+++ programmers_level1/문자열내p_y개수.py
@@ -0,0 +1,12 @@
+def solution(s):
+    p_num = 0
+    y_num = 0
+    for i in s:
+        if i == 'p' or i =='P':
+            p_num += 1
+        elif i == 'y' or i == 'Y':
+            y_num += 1
+    if p_num == y_num:
+        return True
+    else:
+        return False
